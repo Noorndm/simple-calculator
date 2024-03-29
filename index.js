4dcb2cd5779fb49daabdd1a593fbbf8758b33569
Noorndm <abdulnoor89000@gmail.com>
2024-03-29 10:00:45 -0700

    push my calculator to github

--- index.js
+++ index.js
@@ -1,8 +1,8 @@
 #! /usr/bin/env node
 import inquirer from "inquirer";
 const answer = await inquirer.prompt([
-    { message: "Enter Your first number", type: "number", name: "firstNumber" },
-    { message: "Enter Your second number", type: "number", name: "secondNumber" },
+    { message: "Enter Your first number:", type: "number", name: "firstNumber" },
+    { message: "Enter Your second number:", type: "number", name: "secondNumber" },
     {
         message: "Select one of the operator to perform operation",
         type: "list",
